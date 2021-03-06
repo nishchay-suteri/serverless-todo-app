4a51b949f7477e5bd1802551d4afa78c92510b8a
Nishchay Suteri <nishchay271095@gmail.com>
2021-03-06 10:04:37 +0530

    feat: Delete To Do Item with given ID

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,47 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from 'aws-lambda'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+import * as AWS from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODO_TABLE
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  // TODO: User Authentication and user ID
+  console.log('Processing Event: ', event)
   const todoId = event.pathParameters.todoId
 
-  // TODO: Remove a TODO item by id
-  return undefined
+  const key = {
+    todoId: todoId
+  }
+
+  // TODO: Check for invalid todoID ???
+  try {
+    await docClient.delete({ TableName: todoTable, Key: key }).promise()
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: ''
+    }
+  } catch (err) {
+    console.log('DeleteTodo: Error Occurred when creating ToDo')
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: err
+      })
+    }
+  }
 }
