f3dd5fab2736fc8c7adbf8aefeaca26cfeab7956
Nishchay Suteri <nishchay271095@gmail.com>
2021-03-06 21:44:55 +0530

    feat: Update ToDO Item

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,71 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult
+} from 'aws-lambda'
+
+import * as AWS from 'aws-sdk'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODO_TABLE
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  console.log('Processing Event: ', event)
+
+  // TODO: User Authentication ???
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  try {
+    await docClient
+      .update({
+        TableName: todoTable,
+        Key: {
+          todoId: todoId
+        },
+        UpdateExpression:
+          'set #namefield = :name, dueDate = :dueDate, done = :done',
+        ExpressionAttributeNames: {
+          '#namefield': 'name'
+        },
+        ExpressionAttributeValues: {
+          ':name': updatedTodo.name,
+          ':dueDate': updatedTodo.dueDate,
+          ':done': updatedTodo.done
+        },
+        ReturnValues: 'NONE'
+      })
+      .promise()
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: ''
+    }
+  } catch (err) {
+    console.log('UpdateTodo: Error Occurred when creating ToDo')
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: err
+      })
+    }
+  }
+  /*
+{
+  "name": "Buy bread",
+  "dueDate": "2019-07-29T20:01:45.424Z",
+  "done": true
+}
+  */
 }
