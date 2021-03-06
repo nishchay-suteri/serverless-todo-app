bda0884fef3bd91498c7fa98887132f63320fa67
Nishchay Suteri <nishchay271095@gmail.com>
2021-03-06 09:46:12 +0530

    feat: Get All ToDo Items

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,50 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from 'aws-lambda'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+import * as AWS from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODO_TABLE
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+  console.log('Processing Event: ', event)
+
+  try {
+    const result = await docClient
+      .scan({
+        TableName: todoTable
+      })
+      .promise()
+
+    const items = result.Items
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        items
+      })
+    }
+  } catch (err) {
+    console.log('GetTodo: Error Occurred when creating ToDo')
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: err
+      })
+    }
+  }
 }
