7e7cd84bcc6180818dc3fdb7360f7c5a3df8f1cb
Nishchay Suteri <nishchay271095@gmail.com>
2021-03-06 23:33:33 +0530

    feat: Get Signed URL from S3

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,40 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from 'aws-lambda'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+import * as AWS from 'aws-sdk'
+
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
+
+const bucketName = process.env.IMAGES_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  // TODO: Check if todo Item exists
+
+  const url = getUploadURL(todoId)
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      uploadUrl: url
+    })
+  }
+}
+
+const getUploadURL = (todoId: string) => {
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: todoId,
+    Expires: parseInt(urlExpiration)
+  })
 }
