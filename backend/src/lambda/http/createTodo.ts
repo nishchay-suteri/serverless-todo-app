b5bd15ba07d180080cf4b9ee9e364470a1ce1d4b
Nishchay Suteri <nishchay271095@gmail.com>
2021-03-06 09:26:05 +0530

    feat: Create TODO Placeholder Item in DynamoDB

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,77 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult
+} from 'aws-lambda'
+
+import * as AWS from 'aws-sdk'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+import * as uuid from 'uuid'
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODO_TABLE
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  console.log('Processing Event: ', event)
+
+  // TODO: Authenticate user
+
+  const newTodo: CreateTodoRequest = JSON.parse(event.body) // name & dueDate
+
+  const todoId = uuid.v4()
+
+  const createdAt = new Date().toISOString()
+
+  const newItem = {
+    todoId: todoId,
+    createdAt: createdAt,
+    ...newTodo,
+    done: false,
+    attachmentUrl: 'SomeURL' // TODO: Update this
+  }
+
+  try {
+    await docClient
+      .put({
+        TableName: todoTable,
+        Item: newItem
+      })
+      .promise()
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        item: newItem
+      })
+    }
+  } catch (err) {
+    console.log('CreateTodo: Error Occurred when creating ToDo')
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: err
+      })
+    }
+  }
+  /*
+  "item": {
+    "todoId": "123",
+    "createdAt": "2019-07-27T20:01:45.424Z",
+    "name": "Buy milk",
+    "dueDate": "2019-07-29T20:01:45.424Z",
+    "done": false,
+    "attachmentUrl": "http://example.com/image.png"
+  }
+  */
 }
