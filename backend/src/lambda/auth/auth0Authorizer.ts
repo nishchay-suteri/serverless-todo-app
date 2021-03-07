9c389275d94dac8b083e4f439260af46ab7b5a8f
Nishchay Suteri <nishchay271095@gmail.com>
2021-03-07 17:06:14 +0530

    feat: Enable Authentication using Assysmmetric Auth0

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,19 +1,18 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-// import { verify, decode } from 'jsonwebtoken'
+import { verify, decode } from 'jsonwebtoken'
 
 import { createLogger } from '../../utils/logger'
-// import Axios from 'axios'
-// import { Jwt } from '../../auth/Jwt'
-// import { JwtPayload } from '../../auth/JwtPayload'
+import Axios from 'axios'
+import { Jwt } from '../../auth/Jwt'
+import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-// const jwksUrl = '...'
+const jwksUrl = process.env.AUTH0_JWKS_URL
+
+let cachedCertificate
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -24,7 +23,7 @@ export const handler = async (
     logger.info('User was authorized', jwtToken)
 
     return {
-      principalId: 'user', // jwtToken.sub,
+      principalId: jwtToken.sub,
       policyDocument: {
         Version: '2012-10-17',
         Statement: [
@@ -55,19 +54,17 @@ export const handler = async (
   }
 }
 
-// : Promise<JwtPayload>
-async function verifyToken(authHeader: string) {
+async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  // const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  if (token !== '123') {
-    throw new Error('Token is not 123!!!')
+  if (!jwt) {
+    throw new Error('Cannot decode JWT Token')
   }
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  // return undefined
+  const certificate = await getCertificate()
+
+  return verify(token, certificate, { algorithms: ['RS256'] }) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -80,4 +77,39 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+const getCertificate = async () => {
+  if (cachedCertificate) {
+    return cachedCertificate
+  }
+  const data = await Axios.get(jwksUrl)
+  const keys = data.data.keys
+  const signingKeys = keys
+    .filter(
+      (key) =>
+        key.use === 'sig' && // JWK property `use` determines the JWK is for signature verification
+        key.kty === 'RSA' && // We are only supporting RSA (RS256)
+        key.kid && // The `kid` must be present to be useful for later
+        ((key.x5c && key.x5c.length) || (key.n && key.e)) // Has useful public keys
+    )
+    .map((key) => {
+      return {
+        kid: key.kid,
+        nbf: key.nbf,
+        publicKey: certToPEM(key.x5c[0])
+      }
+    })
+
+  if (!signingKeys.length) {
+    throw new Error('No Signing keys found')
+  }
+
+  cachedCertificate = signingKeys[0].publicKey
+  return cachedCertificate
+}
+function certToPEM(cert) {
+  let pem = cert.match(/.{1,64}/g).join('\n')
+  pem = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`
+  return pem
 }
