abe273a2cca10bceee7e409b1e2a92a24dab6e6d
Nishchay Suteri <nishchay271095@gmail.com>
2021-03-07 15:03:40 +0530

    feat: Enable Mock Authentication for all functions

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,18 +1,19 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
+// import { verify, decode } from 'jsonwebtoken'
+
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
-import { JwtPayload } from '../../auth/JwtPayload'
+// import Axios from 'axios'
+// import { Jwt } from '../../auth/Jwt'
+// import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+// const jwksUrl = '...'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -23,7 +24,7 @@ export const handler = async (
     logger.info('User was authorized', jwtToken)
 
     return {
-      principalId: jwtToken.sub,
+      principalId: 'user', // jwtToken.sub,
       policyDocument: {
         Version: '2012-10-17',
         Statement: [
@@ -54,14 +55,19 @@ export const handler = async (
   }
 }
 
-async function verifyToken(authHeader: string): Promise<JwtPayload> {
+// : Promise<JwtPayload>
+async function verifyToken(authHeader: string) {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  // const jwt: Jwt = decode(token, { complete: true }) as Jwt
+
+  if (token !== '123') {
+    throw new Error('Token is not 123!!!')
+  }
 
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  // return undefined
 }
 
 function getToken(authHeader: string): string {
